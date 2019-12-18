7b2b740d7f715f7bb738bb4e9042622b35b6ee32
ochoaangel <44645810+ochoaangel@users.noreply.github.com>
2019-12-18 13:50:31 -0400

    Mejorada buy-your-tickets  my-data my-tickets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,18 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { ClickOutsideModule } from 'ng-click-outside';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+     IonicModule.forRoot(), 
+     AppRoutingModule,
+     ClickOutsideModule
+    
+    ],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/pages/buy-your-ticket/buy-your-ticket.page.ts
+++ src/app/pages/buy-your-ticket/buy-your-ticket.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-buy-your-ticket',
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuyYourTicketPage implements OnInit {
 
-  constructor() { }
+
+  select1show = true;
+  @ViewChild('select1div', { static: false }) select1div: ElementRef;
+
+
+
+
+  constructor(private renderer: Renderer2) {
+
+
+
+    this.renderer.listen('window', 'click', (e: Event) => {
+      if ((this.select1show && e.target !== this.select1div.nativeElement)) {
+        this.select1cerrar();
+      }
+    });
+
+
+
+
+  }
 
   ngOnInit() {
   }
+
+
+  select1() {
+    console.log('origen');
+    this.select1show = !this.select1show;
+  }
+
+
+  select1cerrar() {
+    // console.log('fwfwefwgshrrdhrthrthrthtrh');
+    this.select1show = !this.select1show;
+  }
+
 
 }
