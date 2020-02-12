51048768e3c170c8eff0f64fde5f15e79e2f8a63
unknown <marco.betancourt@clemaber.cl>
2020-02-12 11:04:19 -0300

    Se agrega useHash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -80,7 +80,7 @@ const routes: Routes = [
 @NgModule({
   imports: [ 
     PipesModule,
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules, useHash : true })
   ],
   exports: [RouterModule]
 })
