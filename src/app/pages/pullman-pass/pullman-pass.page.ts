29605b28a042b78ab0a027f88a1b2b30d73b1ca4
ochoaangel <ochoaangel@gmail.com>
2020-07-07 05:09:23 -0400

    ► Mejorada página pullman-pass

--- src/app/pages/pullman-pass/pullman-pass.page.ts
+++ src/app/pages/pullman-pass/pullman-pass.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IntegradorService } from 'src/app/service/integrador.service';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pullman-pass',
@@ -7,28 +9,24 @@ import { IntegradorService } from 'src/app/service/integrador.service';
   styleUrls: ['./pullman-pass.page.scss'],
 })
 export class PullmanPassPage implements OnInit {
+  all;
 
 
   constructor(
-    private integrador: IntegradorService
-  ) { }
+    private integrador: IntegradorService,
+    private router: Router,
 
-  all;
+  ) { }
 
   ngOnInit() {
     this.integrador.getFaq().subscribe(resp => {
       this.all = resp;
-
       this.all.forEach(element => {
         element['show'] = false;
       });
-
-
-      // console.log('this.all', this.all);
     });
-
-
   }
+
 
   ocultarTodo() {
     this.all.forEach(element => {
@@ -36,20 +34,19 @@ export class PullmanPassPage implements OnInit {
     });
   }
 
-  // mostrar(n) {
-  //   this.ocultarTodo();
-  //   this.all[n]['show'] = true;
-  // }
 
   clickItem(n) {
-
     if (this.all[n]['show']) {
       this.all[n]['show'] = false;
     } else {
       this.ocultarTodo()
       this.all[n]['show'] = true;
     }
+  }
 
+
+  btnPullmanPass() {
+    this.router.navigateByUrl('/my-data');
   }
 
 }
